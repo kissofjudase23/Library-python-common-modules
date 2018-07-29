2681505886e23be3c4182a40dc371ddd71157f76
Tom Lin <tom_lin@trend.com.tw>
2018-07-29 22:47:44 +0800

    Add string utility

---
+++ str_utility.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class StrUtility(object):
+
+    @classmethod
+    def to_str(cls, bytes_or_str):
+        """
+
+        :param bytes_or_str:
+        :return: unicode string
+        """
+        if isinstance(bytes_or_str, bytes):
+            value = bytes_or_str.decode('utf-8')
+        else:
+            value = bytes_or_str
+
+        return value
+
+    @classmethod
+    def to_bytes(cls, bytes_or_str):
+        """
+
+        :param bytes_or_str:
+        :return: bytes string
+        """
+        if isinstance(bytes_or_str, str):
+            value = bytes_or_str.encode('utf-8')
+        else:
+            value = bytes_or_str
+
+        return value
