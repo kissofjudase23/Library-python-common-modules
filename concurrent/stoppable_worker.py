699eb1fa31d4a983b1778a4e7602f4f49c50773b
Tom Lin <tom_lin@trend.com.tw>
2018-10-27 14:40:15 +0800

    [concurrent] Add closable_queue and stoppable_worker

---
+++ concurrent/stoppable_worker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from threading import Thread
+
+
+class StoppableWorker(Thread):
+
+    def __init__(self, func, in_queue, out_queue):
+        super().__init__()
+        self.func = func
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+
+    def run(self):
+        for item in self.in_queue:
+            result = self.func(item)
+            self.out_queue.put(result)
