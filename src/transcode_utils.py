e7e4ae9025c30c4b9345fda8b5fddeefcd4ab7e4
Tom Lin <tom_lin@trend.com.tw>
2018-11-04 19:57:38 +0800

    add base64 encode/decode

---
+++ src/transcode_utils.py
@@ -0,0 +1,17 @@
+import base64
+
+
+class TransCoder(object):
+    @staticmethod
+    def decode_base64(b64str, *, encoding='utf-8'):
+        if not b64str:
+            return
+
+        return base64.b64decode(b64str).decode(encoding)
+
+    @staticmethod
+    def encode_base64(str_, *, encoding='utf-8'):
+        if not str_:
+            return
+
+        return base64.b64encode(bytes(str_, encoding)).decode(encoding)
