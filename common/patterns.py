fa2890769e5d9e2a8ea810b687bedce203bb1c76
e23 <kissofjudase23@gmail.com>
2019-04-05 22:16:00 +0800

    add flattern

--- common/patterns.py
+++ common/patterns.py
@@ -1,4 +1,5 @@
 import time
+import collections
 from functools import wraps
 
 
@@ -14,6 +15,21 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+def flatten(d, *, parent_key='', sep='.'):
+
+    items = list()
+
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, parent_key=new_key, sep=sep).items())
+        else:
+            # dictionary item (new_key, v)
+            items.append((new_key, v))
+
+    return dict(items)
 
 
 def retry(exceptions, *, tries=4, delay=1, back_off=2, logger=None):
