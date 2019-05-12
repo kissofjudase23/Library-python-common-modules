85f75abe65e4b00604f6433bdc5922c45ce3469b
Tom Lin <kissofjudase23@gmail.com>
2019-05-12 20:35:00 +0800

    Update linked list

--- common/data_struct/linkedlist.py
+++ common/data_struct/linkedlist.py
@@ -1,4 +1,6 @@
 import sys
+import math
+from abc import ABC, abstractmethod
 
 from ..exc import ArgError
 
@@ -25,6 +27,38 @@ def is_equal(l1, l2):
 
 
 def is_palindrome(l):
+    """
+    time:  O(n)
+    space: O(n)
+    use mid to control flow
+    """
+    len = l.len
+    if len == 0:
+        return False
+    if len == 1:
+        return True
+
+    stack = list()
+    odd = True if len % 2 == 1 else False
+    mid = int(math.ceil(float(len/2)))
+    cnt = 0
+
+    for v in l:
+        cnt += 1
+        if cnt < mid:
+            stack.append(v)
+        elif cnt == mid:
+            if odd:
+                continue  # skip the middle node
+            else:
+                stack.append(v)   # do not have middle node
+        else:
+            if v != stack.pop():
+                return False
+    return True
+
+
+def is_palindrome_v2(l):
     """
     time:  O(n)
     space: O(n)
@@ -59,7 +93,195 @@ def is_palindrome(l):
     return True
 
 
-class LinkedList(object):
+def find_loop_beginning(l):
+    slow_runner = fast_runner = l.head
+    while True:
+        fast_runner = fast_runner.next.next
+        slow_runner = slow_runner.next
+        if fast_runner is slow_runner:
+            break
+
+    target_runner = l.head
+    while True:
+        slow_runner = slow_runner.next
+        target_runner = target_runner.next
+        if target_runner is slow_runner:
+            break
+
+    return target_runner.data
+
+
+def is_palindrome_recursive(l):
+    """
+    time:  O(n)
+    space: O(n)
+    """
+    def is_palindrome(head, len):
+        # stop point, when the length is 0 or 1 means middle node
+        if len == 1:   # odd
+            return True, head.next
+        elif len == 0:  # even
+            return True, head
+
+        ret, compare = is_palindrome(head.next, len-2)
+        if not ret:
+            return False, None
+        if head.data == compare.data:
+            # compare in next round
+            return True, compare.next
+
+        return False, None
+
+    len = l.len
+    if len == 0:
+        return False
+    if len == 1:
+        return True
+
+    ret, _ = is_palindrome(l.head, len)
+    return ret
+
+
+def sum_backward(l1, l2):
+    """
+    time:  O(len(l1) + len(l2))
+    space: O(1)
+    l1 = 7->1->6 = 617
+    l2 = 5->9->2 = 295
+    return 617 + 295 = 912
+    """
+
+    def _sum_backward(l):
+        """
+        7->1->6 = 617
+        7 + 10 + 600 = 617
+        """
+        if l.len < 1:
+            return 0
+
+        val = 0
+        base = 1
+        for i in l:
+            val += (base * i)
+            base *= 10
+        return val
+    return _sum_backward(l1) + _sum_backward(l2)
+
+
+def sum_backward_recursive(l1, l2):
+    """
+    time:  O(len(l1) + len(l2))
+    space: O(max(len(l1), len(l2)))
+
+    l1 = 7->1->6 = 617
+    l2 = 5->9->2 = 295
+    return 617 + 295 = 912
+    """
+
+    def _sum_backward(node, base=1):
+        """
+        7->1->6 = 617
+        7 + 10 + 600 = 617
+        """
+        if not node:
+            return 0
+
+        cur = node.data * base
+        prev = _sum_backward(node.next, base * 10)
+        return cur + prev
+
+    return _sum_backward(l1.head) + _sum_backward(l2.head)
+
+
+def sum_forward(l1, l2):
+    """
+    time:  O(len(l1) + len(l2))
+    space: O(1)
+
+    l1 = 7->1->6 = 716
+    l2 = 5->9->2 = 592
+    return 716 + 592 = 1308
+    """
+
+    def _sum_forward(l):
+        """
+        7->1->6 = 716
+        700 + 10 + 6 = 716
+        """
+        if l.len < 1:
+            return 0
+
+        val = 0
+        for i in l:
+            val = (val * 10) + i
+
+        return val
+
+    return _sum_forward(l1) + _sum_forward(l2)
+
+
+def sum_forward_recursive(l1, l2):
+    """
+    time:  O(len(l1) + len(l2))
+    space: O(max(len(l1), len(l2)))
+
+    l1 = 7->1->6 = 716
+    l2 = 5->9->2 = 592
+    return 716 + 592 = 1308
+    """
+
+    def _sum_forward(node, prev=0):
+        """
+        7->1->6 = 716
+        700 + 10 + 6 = 716
+        """
+        if not node:
+            return prev
+
+        cur = prev * 10 + node.data
+        return _sum_forward(node.next, cur)
+
+    return _sum_forward(l1.head) + _sum_forward(l2.head)
+
+
+class LinkedListABC(ABC):
+
+    @property
+    @abstractmethod
+    def len(self):
+        raise NotImplementedError('please implemet len property')
+
+    @len.setter
+    @abstractmethod
+    def len(self, val):
+        raise NotImplementedError('please implemet len property')
+
+    @abstractmethod
+    def push_front(self, data):
+        raise NotImplementedError('please implemet this method')
+
+    @abstractmethod
+    def push_back(self, data):
+        raise NotImplementedError('please implemet this method')
+
+    @abstractmethod
+    def pop_front(self):
+        raise NotImplementedError('please implemet this method')
+
+    @abstractmethod
+    def pop_back(self):
+        raise NotImplementedError('please implemet this method')
+
+    @abstractmethod
+    def bulk_push_back(self, input_list):
+        raise NotImplementedError('please implemet this method')
+        
+    @abstractmethod
+    def bulk_push_front(self, input_list):
+        raise NotImplementedError('please implemet this method')
+
+
+class LinkedList(LinkedListABC):
     """implement a Linked List:
        push_back : O(1)
        push_front: O(1)
@@ -70,6 +292,36 @@ class LinkedList(object):
         self._len = 0
         self.head = None
         self.tail = None
+
+    def __iter__(self):
+        """ O(n) """
+        runner = self.head
+        while runner:
+            yield runner.data
+            runner = runner.next
+
+    def __eq__(self, other):
+        """ O(n) """
+        if isinstance(other, LinkedList):
+            return is_equal(self, other)
+        return False
+
+    def __ne__(self, other):
+        """ O(n) """
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        """ O(n) """
+        if self.len == 0:
+            return ''
+
+        d_list = list()
+        runner = self.head
+        while runner:
+            d_list.append(str(runner.data))
+            runner = runner.next
+
+        return '->'.join(d_list)
 
     @property
     def len(self):
@@ -141,37 +393,12 @@ class LinkedList(object):
             else:
                 runner = runner.next
 
-    def get_kth_from_front(self, k):
-        if k <= 0:
-            raise ArgError('k must be greater than 1')
-
-        if k > self.len:
-            raise ArgError('out of range')
-
-        runner = self.head
-        for _ in range(k-1):
-            runner = runner.next
-
-        return runner.data
-
-    def get_kth_from_back(self, k):
-        """
-        """
-        if k <= 0:
-            raise ArgError('k must be greater than 1')
-
-        if k > self.len:
-            raise ArgError('out of range')
-
-        forward = self.len - k + 1
-        return self.get_kth_from_front(forward)
-
-    def push_back_bulk(self, input_list):
+    def bulk_push_back(self, input_list):
         """ O(n) """
         for i in input_list:
             self.push_back(i)
 
-    def push_front_bulk(self, input_list):
+    def bulk_push_front(self, input_list):
         """ O(n^2) """
         for i in input_list:
             self.push_front(i)
@@ -196,6 +423,27 @@ class LinkedList(object):
         if prev:
             self.tail = prev
 
+    def remove_middle(self):
+        """ time:  O(n)
+            space: O(n)
+        """
+        if self.len <= 1:
+            return
+
+        fast_runner, slow_runner = self.head, self.head
+        prev = None
+        while fast_runner and fast_runner.next:
+            fast_runner = fast_runner.next
+            # forward 2 steps if possible
+            if fast_runner:
+                fast_runner = fast_runner.next
+
+            prev = slow_runner
+            slow_runner = slow_runner.next
+
+        if prev:
+            prev.next = slow_runner.next
+
     def remove_duplicate(self):
         """ time:  O(n)
             space: O(n)
@@ -210,11 +458,139 @@ class LinkedList(object):
             if cur.data in hash_table:
                 if prev:
                     prev.next = cur.next
+
             else:
                 hash_table[cur.data] = True
                 prev = cur
 
             cur = cur.next
+
+        if prev:
+            self.tail = prev
+
+    def remove_duplicate_space_optimize(self):
+        """ time:  O(n^2) <--- poor performance
+            space: O(1)
+        """
+        if self.len <= 1:
+            return
+
+        slow_runner = self.head
+
+        while slow_runner and slow_runner.next:
+            target = slow_runner.data
+            prev = slow_runner
+            fast_runner = slow_runner.next
+            while fast_runner:
+                if fast_runner.data == target:
+                    prev.next = fast_runner.next
+                else:
+                    prev = fast_runner
+                fast_runner = fast_runner.next
+
+            slow_runner = slow_runner.next
+
+        if prev:
+            self.tail = prev
+
+    def get_kth_from_front(self, k):
+        """ O(n) """
+        if k <= 0:
+            raise ArgError('k must be greater than 1')
+
+        if k > self.len:
+            raise ArgError('out of range')
+
+        runner = self.head
+        for _ in range(k-1):
+            runner = runner.next
+
+        return runner.data
+
+    def get_kth_from_back(self, k):
+        """ O(n) """
+        if k <= 0:
+            raise ArgError('k must be greater than 1')
+
+        if k > self.len:
+            raise ArgError('out of range')
+
+        forward = self.len - (k - 1)
+        return self.get_kth_from_front(forward)
+
+    def get_kth_from_back_iter(self, k):
+        """ Time: O(n)
+            Space: O(1)
+            Assume that self.len is unknown
+        """
+        if k <= 0:
+            raise ArgError('k must be greater than 1')
+
+        fast_runner = self.head
+        for _ in range(k):
+            if not fast_runner:
+                raise ArgError('ouf of range')
+            fast_runner = fast_runner.next
+
+        slow_runner = self.head
+        while fast_runner:
+            fast_runner = fast_runner.next
+            slow_runner = slow_runner.next
+
+        return slow_runner.data
+
+    def get_kth_from_back_recursive(self, k):
+        """ Time:  O(n)
+            Space: O(n),
+            Assume that self.len is unknown
+            ugly code -.-
+        """
+        def _get_kth_from_back_recur(head, k_):
+
+            if not head:  # null
+                return 0, None
+            else:
+                idx, data_ = _get_kth_from_back_recur(head.next, k_)
+                idx += 1
+                if idx == k_:  # kth from the back
+                    return idx, head.data
+                else:
+                    return idx, data_
+
+        if k <= 0:
+            raise ArgError('k must be greater than 1')
+
+        _, data = _get_kth_from_back_recur(self.head, k)
+        if not data:
+            raise ArgError("out of range")
+        else:
+            return data
+
+    def switch_nodes(self):
+        """ O(n)
+            1->2->3->4  ==> 2->1->4->3
+        """
+        if self.len <= 1:
+            return
+
+        # init
+        prev = None
+        cur, next_ = self.head, self.head.next
+        self.head = next_
+
+        while cur and next_:
+            # switch
+            if prev:
+                prev.next = next_
+            cur.next = next_.next
+            next_.next = cur
+
+            # for next round
+            prev = cur
+            cur = cur.next
+            # end condition is cur is None
+            if cur:
+                next_ = cur.next
 
         if prev:
             self.tail = prev
@@ -250,48 +626,130 @@ class LinkedList(object):
         self.head, self.tail = None, None
         self.len = 0
 
+    def partition_stable(self, pivot):
+        """
+        time: O(n)
+        space: O(n)
+        create two linked lists and merge them
+        """
+
+        def append_new_node(head, tail, node):
+            if not head:
+                head = runner
+
+            if tail:
+                tail.next = runner
+
+            tail = runner
+            return head, tail
+
+        """
+        3->5->8->5->10->2->1 ,pivot=5
+        3->2->1->5->8->5->10
+        """
+        if self.len <= 1:
+            return
+
+        before_head, before_tail = None, None
+        after_head, after_tail = None, None
+        runner = self.head
+
+        # create 2 linked list, one is < pivot, another is >= pivot
+        while runner:
+            if runner.data < pivot:
+                before_head, before_tail = append_new_node(before_head, before_tail, runner)
+            else:
+                after_head, after_tail = append_new_node(after_head, after_tail, runner)
+
+            runner = runner.next
+
+        # merge two linked list
+        if after_tail:
+            after_tail.next = None
+
+        if not before_head:
+            self.head = after_head
+            return
+
+        self.head = before_head
+        before_tail.next = after_head
+
+    def partition_non_stable(self, pivot):
+        """
+        time: O(n)
+        space: O(n)
+        """
+        if self.len <= 1:
+            return
+
+        head, tail = None, None
+
+        runner = self.head
+
+        while runner:
+            target = runner
+            runner = runner.next
+            if target.data < pivot:
+                if head:
+                    target.next = head
+                head = target
+                if not tail:  # first node
+                    tail = head
+            else:
+                if tail:
+                    tail.next = target
+                tail = target
+                if not head:  # first node
+                    head = target
+
+        if tail:
+            tail.next = None
+
+        self.head = head
+
     def is_palindrome(self):
+        """
+        time:  O(n)
+        space: O(n)
+        use mid to control flow
+        """
+        return is_palindrome(self)
+
+    def is_palindrome_v2(self):
         """
         time:  O(n)
         space: O(n)
         use fast and flow runner to control flow
         """
-        return is_palindrome(self)
 
-    def __iter__(self):
-        """ O(n) """
-        runner = self.head
-        while runner:
-            yield runner.data
-            runner = runner.next
+        return is_palindrome_v2(self)
 
-    def __eq__(self, other):
-        """ O(n) """
-        if isinstance(other, LinkedList):
-            return is_equal(self, other)
-        return False
+    def is_palindrome_recursive(self):
+        """
+        time:  O(n)
+        space: O(n)
+        """
+        return is_palindrome_recursive(self)
 
-    def __ne__(self, other):
-        """ O(n) """
-        return not self.__eq__(other)
-
-    def __repr__(self):
-        """ O(n) """
-        if self.len == 0:
-            return ''
-
-        d_list = list()
-        runner = self.head
-        while runner:
-            d_list.append(str(runner.data))
-            runner = runner.next
-
-        return '->'.join(d_list)
+    def make_circular(self):
+        """
+        make linkedlist circular
+        1->2->3->1->2->3 .... infinite loop
+        """
+        self.tail.next = self.head
 
 
 def main():
-    pass
+    l1 = LinkedList()
+    l1.bulk_push_back([1, 2, 3])
 
+    l2 = LinkedList()
+    l2.bulk_push_back([100, 200, 300])
+
+    l1.tail.next = l2.head
+    l2.make_circular()
+
+    print(find_loop_beginning(l1))
 
 if __name__ == "__main__":
     sys.exit(main())
--- common/tests/data_struct/test_linkedlist.py
+++ common/tests/data_struct/test_linkedlist.py
@@ -1,6 +1,9 @@
 import pytest
 
 from ...data_struct.linkedlist import LinkedList
+from ...exc import ArgError
+
+from ...data_struct import linkedlist as linkedlist
 
 
 class TestLinkedList(object):
@@ -12,7 +15,7 @@ class TestLinkedList(object):
     ])
     def test_push_back(self, datas, expected):
         ll = LinkedList()
-        ll.push_back_bulk(datas)
+        ll.bulk_push_back(datas)
         assert ll.__repr__() == expected
 
     @pytest.mark.parametrize('datas, expected', [
@@ -22,7 +25,7 @@ class TestLinkedList(object):
     ])
     def test_push_front(self, datas, expected):
         ll = LinkedList()
-        ll.push_front_bulk(datas)
+        ll.bulk_push_front(datas)
         assert ll.__repr__() == expected
 
     @pytest.mark.parametrize('datas, pop_num, expected', [
@@ -33,7 +36,7 @@ class TestLinkedList(object):
     ])
     def test_pop_front(self, datas, pop_num, expected):
         ll = LinkedList()
-        ll.push_back_bulk(datas)
+        ll.bulk_push_back(datas)
 
         for _ in range(pop_num):
             ll.pop_front()
@@ -48,7 +51,7 @@ class TestLinkedList(object):
     ])
     def test_pop_back(self, datas, pop_num, expected):
         ll = LinkedList()
-        ll.push_back_bulk(datas)
+        ll.bulk_push_back(datas)
 
         for _ in range(pop_num):
             ll.pop_back()
@@ -61,8 +64,8 @@ class TestLinkedList(object):
     ])
     def test_equal(self, datas1, datas2):
         l1, l2 = LinkedList(), LinkedList()
-        l1.push_back_bulk(datas1)
-        l2.push_back_bulk(datas2)
+        l1.bulk_push_back(datas1)
+        l2.bulk_push_back(datas2)
 
         assert l1 == l2
 
@@ -72,8 +75,8 @@ class TestLinkedList(object):
     ])
     def test_not_equal(self, datas1, datas2):
         l1, l2 = LinkedList(), LinkedList()
-        l1.push_back_bulk(datas1)
-        l2.push_back_bulk(datas2)
+        l1.bulk_push_back(datas1)
+        l2.bulk_push_back(datas2)
 
         assert l1 != l2
 
@@ -85,7 +88,7 @@ class TestLinkedList(object):
     ])
     def test_reverse(self, datas, expected):
         ll = LinkedList()
-        ll.push_back_bulk(datas)
+        ll.bulk_push_back(datas)
         ll.reverse()
 
         assert ll.__repr__() == expected
@@ -101,9 +104,14 @@ class TestLinkedList(object):
     ])
     def test_remove_duplicate(self, datas, expected):
         l1 = LinkedList()
-        l1.push_back_bulk(datas)
+        l1.bulk_push_back(datas)
         l1.remove_duplicate()
         assert l1.__repr__() == expected
+
+        l2 = LinkedList()
+        l2.bulk_push_back(datas)
+        l2.remove_duplicate_space_optimize()
+        assert l2.__repr__() == expected
 
     @pytest.mark.parametrize('datas, k, expected', [
         pytest.param([1, 3, 5, 7, 9], 1, 1),
@@ -113,7 +121,7 @@ class TestLinkedList(object):
     ])
     def test_get_kth_from_front(self, datas, k, expected):
         ll = LinkedList()
-        ll.push_back_bulk(datas)
+        ll.bulk_push_back(datas)
 
         actual = ll.get_kth_from_front(k)
         assert actual == expected
@@ -126,9 +134,127 @@ class TestLinkedList(object):
     ])
     def test_get_kth_from_back(self, datas, k, expected):
         ll = LinkedList()
-        ll.push_back_bulk(datas)
+        ll.bulk_push_back(datas)
 
-        actual = ll.get_kth_from_back(k)
+        assert ll.get_kth_from_back(k) == expected
+        assert ll.get_kth_from_back_iter(k) == expected
+        assert ll.get_kth_from_back_recursive(k) == expected
+
+    @pytest.mark.parametrize('datas, k', [
+        pytest.param([1, 3, 5, 7, 9], 6),
+        pytest.param([1, 3, 5, 7, 9], 10000000000000),
+        pytest.param([1, 3, 5, 7, 9], -1)
+    ])
+    def test_get_kth_from_back_raise(self, datas, k):
+        ll = LinkedList()
+        ll.bulk_push_back(datas)
+
+        with pytest.raises(ArgError):
+            ll.get_kth_from_back(k)
+
+        with pytest.raises(ArgError):
+            ll.get_kth_from_back_iter(k)
+
+        with pytest.raises(ArgError):
+            ll.get_kth_from_back_recursive(k)
+
+    @pytest.mark.parametrize('datas, expected', [
+        pytest.param([1, 2, 3, 4], "2->1->4->3"),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8, 9], "2->1->4->3->6->5->8->7->9"),
+        pytest.param([1, 2, 3, 4, 5], "2->1->4->3->5"),
+        pytest.param([1], "1")
+    ])
+    def test_switch_nodes(self, datas, expected):
+        ll = LinkedList()
+        ll.bulk_push_back(datas)
+        ll.switch_nodes()
+        assert ll.__repr__() == expected
+
+    @pytest.mark.parametrize('datas, expected', [
+        pytest.param([1], "1"),
+        pytest.param([1, 3, 5], "1->5"),
+        pytest.param([1, 2, 3, 4, 5], "1->2->4->5"),
+    ])
+    def test_remove_nodes(self, datas, expected):
+        ll = LinkedList()
+        ll.bulk_push_back(datas)
+        ll.remove_middle()
+        assert ll.__repr__() == expected
+
+    @pytest.mark.parametrize('datas, pivot, expected', [
+        pytest.param([1, 3, 5], 5, "1->3->5"),
+        pytest.param([3, 5, 8, 5, 10, 2, 1], 5, "3->2->1->5->8->5->10"),
+        pytest.param([3, 5, 8, 5, 10, 2, 1], 11, "3->5->8->5->10->2->1"),
+        pytest.param([3, 5, 8, 5, 10, 2, 1], 0, "3->5->8->5->10->2->1")
+    ])
+    def test_partition_stable(self, datas, pivot, expected):
+        ll = LinkedList()
+        ll.bulk_push_back(datas)
+        ll.partition_stable(pivot)
+        assert ll.__repr__() == expected
+
+    @pytest.mark.parametrize('datas, pivot, expected', [
+        pytest.param([1, 3, 5], 5, "3->1->5"),
+        pytest.param([3, 5, 8, 5, 10, 2, 1], 5, "1->2->3->5->8->5->10"),
+        pytest.param([3, 5, 8, 5, 10, 2, 1], 11, "1->2->10->5->8->5->3"),
+        pytest.param([3, 5, 8, 5, 10, 2, 1], 0, "3->5->8->5->10->2->1")
+    ])
+    def test_partition_non_stable(self, datas, pivot, expected):
+        ll = LinkedList()
+        ll.bulk_push_back(datas)
+        ll.partition_non_stable(pivot)
+        assert ll.__repr__() == expected
+
+    @pytest.mark.parametrize('data1, data2, expected', [
+        pytest.param([7, 1, 6], [5, 9, 2], 912),
+        pytest.param([9, 7, 8], [6, 8, 5], 1465),
+        pytest.param([1, 0, 0], [1, 0, 0, 0, 0], 2),
+        pytest.param([0, 0, 1], [0, 0, 0, 0, 1], 10100)
+    ])
+    def test_sum_backward(self, data1, data2, expected):
+        l1, l2 = LinkedList(), LinkedList()
+        l1.bulk_push_back(data1)
+        l2.bulk_push_back(data2)
+        actual = linkedlist.sum_backward(l1, l2)
+        assert actual == expected
+
+    @pytest.mark.parametrize('data1, data2, expected', [
+        pytest.param([7, 1, 6], [5, 9, 2], 912),
+        pytest.param([9, 7, 8], [6, 8, 5], 1465),
+        pytest.param([1, 0, 0], [1, 0, 0, 0, 0], 2),
+        pytest.param([0, 0, 1], [0, 0, 0, 0, 1], 10100)
+    ])
+    def test_sum_backward_recursive(self, data1, data2, expected):
+        l1, l2 = LinkedList(), LinkedList()
+        l1.bulk_push_back(data1)
+        l2.bulk_push_back(data2)
+        actual = linkedlist.sum_backward_recursive(l1, l2)
+        assert actual == expected
+
+    @pytest.mark.parametrize('data1, data2, expected', [
+        pytest.param([7, 1, 6], [5, 9, 2], 1308),
+        pytest.param([9, 7, 8], [6, 8, 5], 1663),
+        pytest.param([1, 0, 0], [1, 0, 0, 0, 0], 10100),
+        pytest.param([0, 0, 1], [0, 0, 0, 0, 1], 2)
+    ])
+    def test_sum_forward(self, data1, data2, expected):
+        l1, l2 = LinkedList(), LinkedList()
+        l1.bulk_push_back(data1)
+        l2.bulk_push_back(data2)
+        actual = linkedlist.sum_forward(l1, l2)
+        assert actual == expected
+
+    @pytest.mark.parametrize('data1, data2, expected', [
+        pytest.param([7, 1, 6], [5, 9, 2], 1308),
+        pytest.param([9, 7, 8], [6, 8, 5], 1663),
+        pytest.param([1, 0, 0], [1, 0, 0, 0, 0], 10100),
+        pytest.param([0, 0, 1], [0, 0, 0, 0, 1], 2)
+    ])
+    def test_sum_forward_recursive(self, data1, data2, expected):
+        l1, l2 = LinkedList(), LinkedList()
+        l1.bulk_push_back(data1)
+        l2.bulk_push_back(data2)
+        actual = linkedlist.sum_forward_recursive(l1, l2)
         assert actual == expected
 
     @pytest.mark.parametrize('datas, expected', [
@@ -142,6 +268,23 @@ class TestLinkedList(object):
     ])
     def test_is_palindrome(self, datas, expected):
         ll = LinkedList()
-        ll.push_back_bulk(datas)
-        actual = ll.is_palindrome()
-        assert actual is expected
+        ll.bulk_push_back(datas)
+        assert ll.is_palindrome() is expected
+        assert ll.is_palindrome_v2() is expected
+        assert ll.is_palindrome_recursive() is expected
+
+    @pytest.mark.parametrize('data1, data2, expected', [
+        pytest.param([1, 2, 3], [100, 200, 300, 400], 100),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8], [100, 200, 300], 100),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8], [100, 200], 100),
+        pytest.param([1, 2, 3, 4, 5, 6, 7, 8], [100], 100),
+    ])
+    def find_loop_beginning(self, data1, data2, expected):
+            l1 = LinkedList()
+            l1.bulk_push_back(data1)
+            l2 = LinkedList()
+            l2.bulk_push_back(data2)
+            l1.tail.next = l2.head
+            l2.make_circular()
+            actual = linkedlist.find_loop_beginning(l1)
+            assert actual is expected
