cea14b4c6f1ecf2b60c6a26f84ada8429324f7b7
Tom Lin <kissofjudase23@gmail.com>
2019-06-14 15:58:02 +0800

    Add binary search

---
+++ common/algo/search.py
@@ -0,0 +1,60 @@
+import math
+import sys
+
+
+def binary_search(l, target):
+    """
+    Time  Complexity: O(logn)
+    Sapce Complexity: O(1)
+    return -1 if not found
+    """
+
+    start, end = 0, len(l) - 1
+
+    while start <= end:
+        mid = math.ceil((start + end)/2)
+        if target == l[mid]:
+            return mid
+        elif target < l[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return -1
+
+
+def binary_search_recursive(l, target):
+    """
+    Time  Complexity: O(logn)
+    Sapce Complexity: O(logn)
+    return -1 if not found
+    """
+    def _binary_search_recursive(l, start, end, target):
+
+        # out of range
+        if start > end:
+            return -1
+
+        mid = math.ceil((start + end)/2)
+
+        if target == l[mid]:
+            return mid
+
+        elif target < l[mid]:
+            return _binary_search_recursive(l, start, mid - 1, target)
+
+        else:  # target > l[mid]
+            return _binary_search_recursive(l,  mid + 1, end, target)
+
+    start, end = 0, len(l) - 1
+    return _binary_search_recursive(l, start, end, target)
+
+
+def main():
+    sorted_list = [1, 2, 3, 4, 5]
+
+    print(binary_search_recursive(l=sorted_list, target=2))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ common/tests/algo/test_search.py
@@ -0,0 +1,35 @@
+import pytest
+
+from ...algo.search import binary_search, binary_search_recursive
+
+
+
+@pytest.fixture(scope='class')
+def sorted_list():
+    yield [1, 2, 3, 4, 5]
+
+
+class TestBinarySearch(object):
+
+    @pytest.mark.parametrize('target, expected', [
+        pytest.param(1, 0),
+        pytest.param(2, 1),
+        pytest.param(3, 2),
+        pytest.param(4, 3),
+        pytest.param(5, 4),
+        pytest.param(100, -1),
+        pytest.param(-100, -1),
+    ])
+    def test_fast(self, sorted_list, target, expected):
+        assert expected == binary_search(sorted_list, target)
+        assert expected == binary_search_recursive(sorted_list, target)
+
+
+    @pytest.mark.parametrize('sorted_list, target, expected', [
+        pytest.param([0], 0, 0),
+        pytest.param([0, 1], 0, 0),
+        pytest.param([0, 1], 1, 1),
+    ])
+    def test_toft(self, sorted_list, target, expected):
+        assert expected == binary_search(sorted_list, target)
+        assert expected == binary_search_recursive(sorted_list, target)
